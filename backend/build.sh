2446d1074e81ace73029785a566473ce605bfa95
turnyourheadson-star <turnyourheadson@gmail.com>
2025-10-01 23:29:42 +0530

    Updated build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -6,7 +6,7 @@ echo "Setting up Stockfish..."
 mkdir -p stockfish
 
 # Download Stockfish directly from GitHub releases
-STOCKFISH_URL="https://github.com/official-stockfish/Stockfish/releases/download/sf_16/stockfish-ubuntu-x86-64"
+STOCKFISH_URL="https://github.com/official-stockfish/Stockfish/releases/download/sf_16_1/stockfish-ubuntu-20.04-x86-64"
 
 echo "Downloading Stockfish from GitHub..."
 wget -O stockfish/stockfish $STOCKFISH_URL
